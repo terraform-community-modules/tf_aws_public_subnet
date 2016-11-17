656ef729859be7fd29c2c6f59c5c4a447bc583c0
Anton Babenko <anton@antonbabenko.com>
2016-11-17 21:00:24 +0100

    Use aws_route instead of hardcoding routes into aws_route_table to be able to extend (eg, VPN routes)

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ variable "map_public_ip_on_launch" {
   default = true
 }
 
+# Subnet
 resource "aws_subnet" "public" {
   vpc_id                  = "${var.vpc_id}"
   cidr_block              = "${element(var.cidrs, count.index)}"
@@ -32,13 +33,10 @@ resource "aws_subnet" "public" {
   }
 }
 
+# Routes
 resource "aws_route_table" "public" {
   vpc_id = "${var.vpc_id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${var.igw_id}"
-  }
+  count  = "${length(var.cidrs)}"
 
   tags {
     Name = "${var.name}.${element(var.azs, count.index)}"
@@ -46,9 +44,28 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  count          = "${length(var.cidrs)}"
   subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
-  route_table_id = "${aws_route_table.public.id}"
+  route_table_id = "${element(aws_route_table.public.*.id, count.index)}"
+  count          = "${length(var.cidrs)}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route" "igw" {
+  route_table_id         = "${element(aws_route_table.public.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${var.igw_id}"
+  count                  = "${length(var.cidrs)}"
+
+  depends_on             = [
+    "aws_route_table.public"
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 output "subnet_ids" {
